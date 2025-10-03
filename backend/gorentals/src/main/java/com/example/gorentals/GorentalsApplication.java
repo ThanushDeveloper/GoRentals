c606c523c2f7737cdaeea18aaf56fd97c4bd74ef
Thanush <asthanush10@gmail.com>
2025-10-03 19:12:40 +0530

    updated Backend

---
+++ backend/gorentals/src/main/java/com/example/gorentals/GorentalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gorentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorentalsApplication.class, args);
+	}
+
+}
